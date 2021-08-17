593d35ea4dfe27d65660a807096e116c726e12ce
butch <au.bouchard@gmail.com>
2021-08-17 22:59:43 +0200

    post show like, time, separate date

--- the_coffee_place/tcp-frontend-app/src/components/Flow.jsx
+++ the_coffee_place/tcp-frontend-app/src/components/Flow.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 
 import Redactor from "./Redactor";
 import Post from "./Post";
+import ScrollToTop from "./ScrollToTop";
 
 
 const fakePosts = [
-    {username:"toto", post:"C'est l'histoire d'une sardine qui rentre dans un bar"},
-    {username:"KeuV'1", post:"Hier je mais acheter une Mégane RS, en bonne étas ! Ces tros de la bombe cet baniol"},
-    {username:"JP (de la compta)", post:"Quelqu'un sait où se trouve mon bureau ? je veux dire, à part moi."},
-    {username:".", post:".."},
-    {username:"JEAN-JEAN", post:"APERO ?\n"+"APERO ! APERO ! APERO !\n"+"APEROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n"+"et glou"},
-    {username:"WikiBot", post:"En géométrie euclidienne, un cercle est une courbe plane fermée constituée des points situés à égale distance d'un point nommé centre. La valeur de cette distance est appelée rayon du cercle.\n" +
+    {key:"001", date:"15/08/2021", clock:"12:35:00", liked:true, username:"toto", post:"C'est l'histoire d'une sardine qui rentre dans un bar"},
+    {key:"002", date:"15/08/2021", clock:"13:22:00", liked:true, username:"KeuV'1", post:"Hier je mais acheter une Mégane RS, en bonne étas ! Ces tros de la bombe cet baniol"},
+    {key:"003", date:"16/08/2021", clock:"07:01:00", liked:false, username:"JP (de la compta)", post:"Quelqu'un sait où se trouve mon bureau ? je veux dire, à part moi."},
+    {key:"004", date:"16/08/2021", clock:"12:35:00", liked:false, username:".", post:".."},
+    {key:"005", date:"16/08/2021", clock:"17:59:00", liked:true, username:"JEAN-JEAN", post:"APERO ?\n"+"APERO ! APERO ! APERO !\n"+"APEROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n"+"et glou"},
+    {key:"006", date:"17/08/2021", clock:"16:12:00", liked:false, username:"WikiBot", post:"En géométrie euclidienne, un cercle est une courbe plane fermée constituée des points situés à égale distance d'un point nommé centre. La valeur de cette distance est appelée rayon du cercle.\n" +
             "\n" +
             "Dans le plan euclidien, il s'agit du « rond » qui est associé en français au terme de cercle. Dans un plan non euclidien ou dans le cas de la définition d'une distance non euclidienne, la forme peut être plus complexe. Dans un espace de dimension quelconque, l'ensemble des points placés à une distance constante d'un centre est appelé sphère.\n" +
             "\n" +
@@ -22,9 +23,15 @@ export default function Flow() {
         <div className="mx-auto pb-8 w-5/6">
             <Redactor />
             <>
-                { fakePosts.map(({username, post}) =>{
+                { fakePosts.map(({key, date, clock, liked, username, post}) =>{
                     return (
-                        <Post fakeUsername={username} fakeText={post}/>
+                        <Post
+                            key={key}
+                            date={date}
+                            clock={clock}
+                            liked={liked}
+                            fakeUsername={username}
+                            fakeText={post}/>
                     )
                 })
 
--- the_coffee_place/tcp-frontend-app/src/components/Post.jsx
+++ the_coffee_place/tcp-frontend-app/src/components/Post.jsx
@@ -2,26 +2,84 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-export default function Post({fakeText, fakeUsername}) {
-    const time = new Date;
+export default function Post({date, clock, liked, fakeText, fakeUsername}) {
+
     return (
-                <div
-                    className="my-4 p-3 flex flex-col rounded-3xl bg-white"
-                    onClick={
-                /*        will handle large view with router.redirect and router.parameters     */
-                        null}
-                >
-                    <div className="bg-blue-gray-100 rounded-2xl px-1">
-                        <p
-                            className="handWritten text-2xl
+        <>
+            {date ? <div className="bg-gray-500 w-full mx-auto">{date}</div> : null}
+            <div
+                className="my-4 p-3 flex flex-col rounded-3xl bg-white"
+                onClick={
+                    /*        will handle large view with router.redirect and router.parameters     */
+                    null}
+            >
+                <div className="bg-blue-gray-100 rounded-2xl px-1">
+                    <p
+                        className="handWritten text-2xl
                             overflow-ellipsis overflow-hidden whitespace-pre-line">{fakeText}</p>
-                    </div>
-                    <div className="footer flex flex-row justify-between items-end">
-                        <p className="mono mt-2">{"Le "+(time.toLocaleString()).split(',')[0]}</p>
-                        <p className="handWritten mt-2 text-2xl">{fakeUsername}</p>
-                    </div>
-
-
                 </div>
+                <div className="footer flex flex-row justify-between items-end">
+                    <p className="mono mt-2">{clock}</p>
+                    <div className="mt-2 flex flex-row ">
+                        <span className="handWritten text-2xl">{fakeUsername}</span>
+                        {liked ? <span className="liked ml-2 h-8 text-yellow-700 text-xl"><i className="fas fa-thumbs-up"></i></span>
+                            : <span className="notliked ml-2 pt-1 h-8 text-gray-300"><i className="fas fa-thumbs-up"></i></span> }
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
+
+
+/*
+* time formating :
+*     const time = new Date;
+* time.toLocaleString('fr-FR')).split(',')[0]
+* */
+
+
+const badWords = [
+    "con",
+    "cons",
+    "conne",
+    "connes",
+    "connard",
+    "connards",
+    "connasse",
+    "connasses",
+    "salop",
+    "salops",
+    "salope",
+    "salopes",
+    "pute",
+    "putes",
+    "pd",
+    "pédé",
+    "pédés",
+    "enculé",
+    "enculée",
+    "enculés",
+    "enculées",
+    "putain",
+    "putains",
+    "bite",
+    "bites",
+    "couilles",
+    "couille",
+    "branleur",
+    "branleurs",
+    "saloperie",
+    "saloperies",
+    "foutre",
+    "foutres",
+    "crétin",
+    "crétins",
+    "crétine",
+    "crétines",
+    "couillon",
+    "couillons",
+    "couillone",
+    "couillones"
+]
