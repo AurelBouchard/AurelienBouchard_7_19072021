813ae3a319199d34cc516736d89462a530de609c
butch <au.bouchard@gmail.com>
2021-08-27 08:58:22 +0200

    profile edition 90% of formik

--- the_coffee_place/tcp-frontend/src/components/ProfileEditor.jsx
+++ the_coffee_place/tcp-frontend/src/components/ProfileEditor.jsx
@@ -22,67 +22,119 @@ export default function ProfileEditor({currentUser}) {
 
                 <Formik
                     initialValues={{
+                        //imgUrl:     data.user.imgUrl,
                         pseudo:     data.user.pseudo,
                         password:   data.user.password,
                         firstName:  data.user.firstName,
                         lastName:   data.user.lastName,
                         email:      data.user.email,
-                        about:      data.user.about,
+                        about:      data.user.about || "Quelques mot sur vous",
                     }}
-                    validate={values => {
+
+                /*    validate={values => {
                         const errors = {};
                         if (!values.pseudo) { errors.pseudo = 'Champ requis'; }
                         if (!values.password) { errors.password = 'Champ requis'; }
                         return errors;
+                    }}*/
+
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+
+                            console.log("try to modify account ...");
+
+                            const payload = {
+                                //imgUrl:     data.user.imgUrl,
+                                pseudo:     data.user.pseudo,
+                                firstName:  data.user.firstName,
+                                lastName:   data.user.lastName,
+                                email:      data.user.email,
+                                about:      data.user.about
+                            }
+
+                            axios.put(`http://localhost:4000/api/auth/update/${data.user.id}`, payload)
+                                .then(function (response) {
+                                    //console.log(response.data);
+                                    console.log(response.status);
+                                    //console.log(response.statusText);
+                                    console.log(response.headers);
+                                    console.log(response.config);
+                                    //setState( {data: response, loading: false} );
+
+                                })
+                                .then(() => {
+                                    console.log("Profil modifié avec succes")
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                });
+
+
+                            setSubmitting(false);
+                        }, 400);
                     }}
                 >
 
-                    <Form>
-                        <div className="editor flex flex-col pt-4 md:p-8 mx-auto items-center
-                md:flex-row flex-wrap md:justify-center md:max-w-2xl">
-                            <img
-                                className="rounded-2xl bg-white md:m-8 md:self-start "
-                                src="src/assets/coffee-cup-192.png"
-                                alt="Profile picture"
-                                max-width={200} max-height={200}    // md: 200 -> 300
-                                onClick={ () => { alert("Choisissez une photo")}
-                                }
-                            />
+                    {({ isSubmitting }) => (
 
-                            <div className="names flex flex-col m-4 md:m-8">
-                                <Input type={"text"} label={"Pseudo"} placeholder={data.user.pseudo} />
-                                <Input type={"text"} label={"Prénom"} placeholder={data.user.firstName} />
-                                <Input type={"text"} label={"Nom"} placeholder={data.user.lastName} />
-                                <Input type={"email"} label={"Email"} placeholder={data.user.email} />
-                            </div>
+                        <Form>
+                            <div className="editor flex flex-col pt-4 md:p-8 mx-auto items-center
+                    md:flex-row flex-wrap md:justify-center md:max-w-2xl">
+                                <img
+                                    className="rounded-2xl bg-white md:m-8 md:self-start "
+                                    src="src/assets/coffee-cup-192.png"
+                                    alt="Profile picture"
+                                    max-width={200} max-height={200}    // md: 200 -> 300
+                                    onClick={ () => { alert("Choisissez une photo")}
+                                    }
+                                />
 
-                            <div className="intro flex flex-col m-4 md:w-full ">
-                                {data.user.about ?
-                                    <Input type={"textarea"} label={"Présentation"} value={data.user.about}/>
-                                    :
-                                    <Input type={"textarea"} label={"Présentation"} placeholder={"Quelques mots sur vous ..."}/>}
+                                <div className="names flex flex-col m-4 md:m-8">
+                                    {/*<Input type={"text"} label={"Pseudo"} placeholder={data.user.pseudo} />*/}
+                                    <Field id="pseudo" name="pseudo" />
+                                    <ErrorMessage name="pseudo" component="div" />
 
-                            </div>
+                                    {/*<Input type={"text"} label={"Prénom"} placeholder={data.user.firstName} />*/}
+                                    <Field id="firstName" name="firstName" />
+                                    <ErrorMessage name="firstName" component="div" />
 
-                            <div className="admin flex flex-col m-4 mx-16 md:mx-4 md:w-full">
-                                <p>Gestion du compte :</p>
-                                <div className="md:flex md:flex-row md:justify-around">
-                                    <Button text="Changer le mot de passe">
+                                    {/*<Input type={"text"} label={"Nom"} placeholder={data.user.lastName} />*/}
+                                    <Field id="lastName" name="lastName" />
+                                    <ErrorMessage name="lastName" component="div" />
+
+                                    {/*<Input type={"email"} label={"Email"} placeholder={data.user.email} />*/}
+                                    <Field type="email" id="email"name="email" />
+                                    <ErrorMessage name="email" component="div" />
+                                </div>
+
+                                <div className="intro flex flex-col m-4 md:w-full ">
+                                    {/*<Input type={"textarea"} label={"Présentation"} placeholder={"Quelques mots sur vous ..."}/>*/}
+                                    <Field type='undefined' id="about"name="about" />
+                                    <ErrorMessage name="about" component="div" />
+                                </div>
+
+                                <div className="admin flex flex-col m-4 mx-16 md:mx-4 md:w-full">
+                                    <p>Gestion du compte :</p>
+                                    <div className="md:flex md:flex-row md:justify-around">
+                                        <Button text="Changer le mot de passe">
+                                        </Button>
+                                        <Button text="Supprimer le compte">
+                                        </Button>
+                                    </div>
+                                </div>
+
+                                <div className="btns flex flex-row w-full justify-evenly my-8">
+                                    <Button text="Retour" type='reset'
+                                            onClick={() => {// use history
+                                                 }}>
                                     </Button>
-                                    <Button text="Supprimer le compte">
+                                    <Button text="Valider" type='submit'>
                                     </Button>
                                 </div>
                             </div>
 
-                            <div className="btns flex flex-row w-full justify-evenly my-8">
-                                <Button text="Retour" type='reset'>
-                                </Button>
-                                <Button text="Valider" type='submit'>
-                                </Button>
-                            </div>
-                        </div>
-
-                    </Form>
+                        </Form>
+                    )}
 
 
                 </Formik>
