bc366e62392d2e3c0fb23d7a992ccbd43d8eee43
butch <au.bouchard@gmail.com>
2022-07-26 15:49:20 +0200

    sequelize use dotenv better

--- the_coffee_place/tcp-backend/db_management/sequelize.js
+++ the_coffee_place/tcp-backend/db_management/sequelize.js
@@ -1,14 +1,16 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
-/*    define: {
-        freezeTableName: true
-    },*/
-    dialect: "mysql",
-
-    host: process.env.DB_HOST,
-    port: process.env.DB_port
-});
+const sequelize = new Sequelize(
+    `${process.env.DB_NAME}`,
+    `${process.env.DB_USER}`,
+    `${process.env.DB_PASSWORD}`,
+    {
+        dialect: "mysql",
+        host: process.env.DB_HOST,
+        port: process.env.DB_port
+    });
 
 module.exports = sequelize;
+
+
